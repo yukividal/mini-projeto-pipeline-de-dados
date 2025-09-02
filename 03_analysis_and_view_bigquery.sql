2f70a284cf46957bfda7b1cd419e03131255e6cd
yukividal <112736200+yukividal@users.noreply.github.com>
2025-09-02 19:26:33 -0300

    Update 03_analysis_and_view_bigquery.sql

--- 03_analysis_and_view_bigquery.sql
+++ 03_analysis_and_view_bigquery.sql
@@ -5,12 +5,12 @@
 
 -- Pergunta 1: Qual o nome dos clientes que moram no estado de 'SP'?
 SELECT Nome_Cliente
-FROM `seu-projeto.seu_dataset.Clientes`
+FROM `t1engenhariadados.Livraria_DevSaber_Grupo_3_9.Clientes`
 WHERE Estado_Cliente = 'SP';
 
 -- Pergunta 2: Quais produtos pertencem à categoria 'Ficção Científica'?
 SELECT Nome_Produto
-FROM `seu-projeto.seu_dataset.Produtos`
+FROM `t1engenhariadados.Livraria_DevSaber_Grupo_3_9.Produtos`
 WHERE Categoria_Produto = 'Ficção Científica';
 
 -- Pergunta 3: Listar todas as vendas, mostrando o nome do cliente, o nome do produto e a data da venda. Ordene pela data.
@@ -18,25 +18,25 @@ SELECT
     C.Nome_Cliente,
     P.Nome_Produto,
     V.Data_Venda
-FROM `seu-projeto.seu_dataset.Vendas` AS V
-JOIN `seu-projeto.seu_dataset.Clientes` AS C ON V.ID_Cliente = C.ID_Cliente
-JOIN `seu-projeto.seu_dataset.Produtos` AS P ON V.ID_Produto = P.ID_Produto
+FROM `t1engenhariadados.Livraria_DevSaber_Grupo_3_9.Vendas` AS V
+JOIN `t1engenhariadados.Livraria_DevSaber_Grupo_3_9.Clientes` AS C ON V.ID_Cliente = C.ID_Cliente
+JOIN `t1engenhariadados.Livraria_DevSaber_Grupo_3_9.Produtos` AS P ON V.ID_Produto = P.ID_Produto
 ORDER BY V.Data_Venda;
 
 -- Pergunta 4: Qual o valor total de cada venda? (quantidade * preço)
 SELECT
     V.ID_Venda,
     (V.Quantidade * P.Preco_Produto) AS Valor_Total
-FROM `seu-projeto.seu_dataset.Vendas` AS V
-JOIN `seu-projeto.seu_dataset.Produtos` AS P ON V.ID_Produto = P.ID_Produto;
+FROM `t1engenhariadados.Livraria_DevSaber_Grupo_3_9.Vendas` AS V
+JOIN `t1engenhariadados.Livraria_DevSaber_Grupo_3_9.Produtos` AS P ON V.ID_Produto = P.ID_Produto;
 
 
 -- Pergunta 5: Qual o produto mais vendido em termos de quantidade?
 SELECT
     P.Nome_Produto,
     SUM(V.Quantidade) AS Total_Quantidade_Vendida
-FROM `seu-projeto.seu_dataset.Vendas` AS V
-JOIN `seu-projeto.seu_dataset.Produtos` AS P ON V.ID_Produto = P.ID_Produto
+FROM `t1engenhariadados.Livraria_DevSaber_Grupo_3_9.Vendas` AS V
+JOIN `t1engenhariadados.Livraria_DevSaber_Grupo_3_9.Produtos` AS P ON V.ID_Produto = P.ID_Produto
 GROUP BY P.Nome_Produto
 ORDER BY Total_Quantidade_Vendida DESC
 LIMIT 1;
@@ -44,7 +44,7 @@ LIMIT 1;
 
 -- CRIAÇÃO DA VIEW --
 -- Criar uma view para simplificar o acesso a um relatório detalhado de vendas.
-CREATE OR REPLACE VIEW `seu-projeto.seu_dataset.v_relatorio_vendas_detalhado` AS
+CREATE OR REPLACE VIEW `t1engenhariadados.Livraria_DevSaber_Grupo_3_9.v_relatorio_vendas_detalhado` AS
 SELECT
     V.ID_Venda,
     V.Data_Venda,
@@ -55,7 +55,7 @@ SELECT
     V.Quantidade,
     P.Preco_Produto,
     (V.Quantidade * P.Preco_Produto) AS Valor_Total
-FROM `seu-projeto.seu_dataset.Vendas` AS V
+FROM `t1engenhariadados.Livraria_DevSaber_Grupo_3_9.Vendas` AS V
 JOIN `seu-projeto.seu_dataset.Clientes` AS C ON V.ID_Cliente = C.ID_Cliente
 JOIN `seu-projeto.seu_dataset.Produtos` AS P ON V.ID_Produto = P.ID_Produto;
 
